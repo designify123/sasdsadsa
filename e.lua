b950882f91532385d2adb0e2fb874df986fb3fd5
zov24 <79064796+designify123@users.noreply.github.com>
2021-10-17 17:01:21 +0100

    Update e.lua

--- e.lua
+++ e.lua
@@ -1,4 +1,5 @@
 local speaker = game.Players.LocalPlayer
+print("Version 1.0")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source", true))()
 local people = {
     "432757113"
