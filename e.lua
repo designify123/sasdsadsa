fedf4a386a8cff9d6cf6b47c18cb7c3371ed77db
zov24 <79064796+designify123@users.noreply.github.com>
2021-10-17 16:22:49 +0100

    Update e.lua

--- e.lua
+++ e.lua
@@ -37,8 +37,10 @@ end
             game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Adam is best, brady wady is adam's cat maid.", "All")
         end
         if msg == "/e fakekick" then 
-            speaker:Kick("Unkown client actions detected, your account has been flagged. ")
+            speaker:Kick("Unkown client actions detected, your account has been flagged.")
         end
+        if msg == "/e ct2" then 
+             game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("men <3", "All")
     end)
 end
 
